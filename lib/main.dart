3ab0d4e404dd4b10407332c9c890c91a5a5b6cd3
kimjuii <cathy033077@gmail.com>
2022-08-10 13:03:57 +0900

    Hompage 위젯 Responsive 되도록 수정, Drawer 추가
    Code 정리 필요, GridView 사용하는 방향 시도할 필요 있음,

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:calculator_frontend/CapitalGainsTax.dart';
 import 'package:calculator_frontend/HoldingTax.dart';
-import 'package:calculator_frontend/widgets/LargeText.dart';
-import 'package:calculator_frontend/widgets/NavigationBox.dart';
+import 'package:calculator_frontend/widgets/HomePage/LargeLayout.dart';
+import 'package:calculator_frontend/widgets/HomePage/MediumLayout.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,117 +16,36 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'TAXAI',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primaryColor: Color(0xff80cfd5),
       ),
-      home: const MyHomePage(title: 'TAXAI Home Page'),
+      home: MyHomePage(
+          MediumLayout: const MediumLayout(), LargeLayout: const LargeLayout()),
       routes: {
-        '/row1col1': (context) => CapitalGainsTaxPage(),
-        '/row1col2': (context) => HoldingTaxPage()
+        '/capgain': (context) => CapitalGainsTaxPage(),
+        '/holding': (context) => HoldingTaxPage()
       },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class MyHomePage extends StatelessWidget {
+  final Widget MediumLayout;
+  final Widget LargeLayout;
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final mainColor = 0xff80cfd5;
-  late double widget_width;
-  late double widget_height;
+  const MyHomePage(
+      {Key? key, required this.MediumLayout, required this.LargeLayout})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    widget_width = MediaQuery.of(context).size.width;
-    widget_height = MediaQuery.of(context).size.height;
-    return Scaffold(
-        body: SafeArea(
-      child: Padding(
-        padding:
-            EdgeInsets.only(left: widget_width * .1, right: widget_width * .1),
-        child: Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 70,
-              ),
-              LargeText(
-                text: 'AI 세금 계산',
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  NavigationBox(
-                      pushNamed: '/row1col1',
-                      title_1: '양도소득세',
-                      title_2: 'AI 판단 계산기'),
-                  NavigationBox(
-                      pushNamed: '/row1col2',
-                      title_1: '보유세(종부세, 재산세)',
-                      title_2: 'AI 판단 계산기'),
-                  NavigationBox(
-                    pushNamed: '/row1col1',
-                    title_1: '',
-                    title_2: '',
-                    iconColor: Colors.transparent,
-                    borderColor: Colors.transparent,
-                    boxColor: Colors.transparent,
-                  ),
-                  NavigationBox(
-                    pushNamed: '/row1col1',
-                    title_1: '',
-                    title_2: '',
-                    iconColor: Colors.transparent,
-                    borderColor: Colors.transparent,
-                    boxColor: Colors.transparent,
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 150,
-              ),
-              LargeText(
-                text: 'TAXAI 컨설팅',
-              ),
-              const SizedBox(
-                height: 40,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  NavigationBox(
-                      pushNamed: '/row1col1',
-                      title_1: '양도소득세 AI',
-                      title_2: '컨설팅'),
-                  NavigationBox(
-                      pushNamed: '/row1col1',
-                      title_1: '매도 관련',
-                      title_2: 'AI 컨설팅'),
-                  NavigationBox(
-                      pushNamed: '/row1col1',
-                      title_1: '매입 관련',
-                      title_2: 'AI 컨설팅'),
-                  NavigationBox(
-                      pushNamed: '/row1col1',
-                      title_1: '증여 관련',
-                      title_2: 'AI 컨설팅')
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    ));
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth < 1200) {
+        return MediumLayout;
+      } else {
+        return LargeLayout;
+      }
+    });
   }
 }
